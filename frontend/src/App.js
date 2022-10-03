2c03c188ce6a97e3eec3fd41b943ae9cd9baf15a
nikesh777github <nikeshrthd777@gmail.com>
2022-10-03 16:28:39 +0530

    firsdt

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">City Deals</a>
+      </header>
+      <main>List Of Products ...</main>
+      <xyz>OKkkkkk</xyz>
+    </div>
+  );
+}
+
+export default App;
